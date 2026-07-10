6db6b48e12eb5dc7ea46cde35b0e5fde6b5a9042
69070260 <69070260@kmitl.ac.th>
2026-07-10 13:02:27 +0700

    เพิ่มเยอะแยะ

---
+++ 3005/กระต่าย.py
@@ -0,0 +1,7 @@
+"""doc"""
+
+x, y, z = input().split(" ")
+x1 = int(x)*10
+x2 = int(y)*25
+x3 = int(z)*3
+print(f"{x1 + x2 + x3}")
---
+++ EuclideanDistance2D.py
@@ -0,0 +1,8 @@
+"""โปรแกรมรับค่าตัวเลข 4 ตัว"""
+import math
+q1 = input()
+q2 = input()
+p1 = input()
+p2 = input()
+
+print()
