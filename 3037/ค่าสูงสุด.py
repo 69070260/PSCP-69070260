9a0771f9f051b3683762daf6a2a641b9170546b1
69070260 <69070260@kmitl.ac.th>
2026-07-17 12:52:29 +0700

    ทำงาน วีค 3

---
+++ 3037/ค่าสูงสุด.py
@@ -0,0 +1,24 @@
+"""โปรแกรมหาค่าสูงสุด"""
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+if a == b == c:
+    print(a)
+elif (a >= b) and (a >= c):
+    if a > c or a > b:
+        print(a)
+    elif a < c:
+        print(c)
+    elif a < b:
+        print(b)
+elif (b >= a) and (b >= c):
+    if b > c or b > a:
+        print(b)
+    elif b < c:
+        print(c)
+    elif b < a:
+        print(a)
+elif (c >= a) and (c >= b):
+    print(c)
