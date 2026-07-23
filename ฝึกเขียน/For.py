8e844f9ef91b263c030aced7417033978986c45e
69070260 <69070260@kmitl.ac.th>
2026-07-24 00:32:51 +0700

    ทำอะไรหลายๆอย่าง

---
+++ ฝึกเขียน/For.py
@@ -0,0 +1,6 @@
+"""ฝึก for loop"""
+for counter in range(1,5): # 0 - 4
+    print(counter)
+
+for counter in range(10,0,-1): # 10 - 1
+    print(counter)
---
+++ ฝึกเขียน/While.py
@@ -0,0 +1,7 @@
+"""ฝึกเขียน While"""
+
+counter = 100
+while counter < 3:
+    counter += 1
+    print(counter)
+print("จบการทำงาน")
