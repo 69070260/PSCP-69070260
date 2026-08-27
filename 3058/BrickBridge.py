b852a36b689b42e47ba62ab9e0cc8dc188c6bb3a
69070260 <69070260@kmitl.ac.th>
2026-08-27 20:46:06 +0700

    เพิ่ม submission

---
+++ 3058/BrickBridge.py
@@ -0,0 +1,17 @@
+"""โปรแกรมก่อสพาน"""
+
+a = int(input())
+b = int(input())
+goal = int(input())
+
+big = goal // 5
+
+if big > b:
+    big = b
+
+remain = goal - (big * 5)
+
+if remain <= a:
+    print(remain)
+else:
+    print(-1)
--- NOT LEARNING LOGS WEEK4/3064/วันเกิด.py
+++ NOT LEARNING LOGS WEEK4/3064/วันเกิด.py
@@ -1,5 +1,7 @@
 """โปรแกรมคำนวนว่าใครเกิดก่อน"""
 
+from datetime import date
+
 y1 = int(input())
 m1 = int(input())
 d1 = int(input())
@@ -8,14 +10,14 @@ y2 = int(input())
 m2 = int(input())
 d2 = int(input())
 
-day1 = y1 * 365 + m1 * 30 + d1
-day2 = y2 * 365 + m2 * 30 + d2
+date1 = date(y1, m1, d1)
+date2 = date(y2, m2, d2)
 
-diff = day1 - day2
+diff = abs((date1 - date2).days)
 
-if -7 <= diff <= 7:
+if diff <= 7:
     print(0)
-elif diff < 0:
+elif date1 < date2:
     print(1)
 else:
     print(2)
